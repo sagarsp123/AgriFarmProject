b4cf7b1c1f1fdfd852ae154c47b63e57e2113a0c
Vijaypvs <pvs452@gmail.com>
2020-12-07 19:49:17 +0530

    minor changes in code

--- AgriFarmProj/src/app/bidder-auction/bidder-auction.component.ts
+++ AgriFarmProj/src/app/bidder-auction/bidder-auction.component.ts
@@ -11,6 +11,8 @@ import { NgForm } from '@angular/forms';
 export class BidderAuctionComponent implements OnInit {
 
 
+  imagetodisplay=null;
+
  auctionprice:AuctionBid;
   //currentcrops:any=[];
   currentcrop:any=[];
@@ -19,6 +21,8 @@ export class BidderAuctionComponent implements OnInit {
       BidPrice:0
     }
   }
+
+
 
   ngOnInit(): void {
     //debugger;
@@ -30,6 +34,14 @@ export class BidderAuctionComponent implements OnInit {
     this.currentcrop=this.biddermarketplaceservice.getCurrentCropById().subscribe((data)=>{
       this.currentcrop=data;console.log(data);
       console.log(this.currentcrop);
+      if(this.currentcrop.CropName=="Rice") this.imagetodisplay="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse4.mm.bing.net%2Fth%3Fid%3DOIP.yEHGAXlXb5cEtpN_r6teaAHaE8%26pid%3DApi&f=1";
+      else if(this.currentcrop.CropName =="Cotton") this.imagetodisplay="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.vF-kIlVf1ekJ2e6ZxpByjQHaE6%26pid%3DApi&f=1";
+      else if(this.currentcrop.CropName =="GroundNut") this.imagetodisplay="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse4.mm.bing.net%2Fth%3Fid%3DOIP.q1Vd-fG4MwH8i2jhhZoFjgHaE8%26pid%3DApi&f=1";
+      else if(this.currentcrop.CropName =="SoyaBean") this.imagetodisplay="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.yxnDXSmKInso8gSqe-rm8AHaI8%26pid%3DApi&f=1";
+      else if(this.currentcrop.CropName =="Wheat") this.imagetodisplay="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse2.mm.bing.net%2Fth%3Fid%3DOIP.Ym22T14TuJpD3DFQgHKjwgHaFL%26pid%3DApi&f=1";
+      else if(this.currentcrop.CropName =="Barley") this.imagetodisplay="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.obUGrjcf7QllQ-kwgzdFsQHaEK%26pid%3DApi&f=1";
+      else if(this.currentcrop.CropName =="Ginger") this.imagetodisplay="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse3.mm.bing.net%2Fth%3Fid%3DOIP.m-A98TZy1eoZouIbr0Hd2wHaET%26pid%3DApi&f=1";
+      else if(this.currentcrop.CropName =="Pea") this.imagetodisplay="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.4lay8v6Gu1DUOVgIZoY6cwHaGg%26pid%3DApi&f=1";
     });
   }
 
